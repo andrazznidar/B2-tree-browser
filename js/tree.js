a11e5a6b0fd76b031f5b4099c2193b74b373dbe2
Andraž Žnidar <andrazznidar@users.noreply.github.com>
2020-10-08 19:40:39 +0200

    New preview on large devices

--- js/tree.js
+++ js/tree.js
@@ -6,7 +6,52 @@ var filetype = "";
 var foldername = "";
 var path = "";
 
-// This function 'traverse(obj)' is modified from a snippet published on the website stackoverflow (https://stackoverflow.com/a/10460119) and as such is licensed as CC BY-SA 4.0 (https://creativecommons.org/licenses/by-sa/4.0/). The original stackoverflow anwser was made by El Ronnoco (https://stackoverflow.com/users/395628/el-ronnoco) that was additionally modified by user1371896 (https://stackoverflow.com/users/1371896/user1371896) and OCDev (https://stackoverflow.com/users/508558/ocdev).
+const imageFileExtension = [
+  "gif",
+  "jpeg",
+  "jpg",
+  "png",
+  "svg",
+  "svgz",
+  "tif",
+  "tiff",
+  "wbmp",
+  "webp",
+  "ico",
+  "jng",
+  "bmp",
+];
+
+// This const and the folowing function 'encodeS3URI(filename)' is modified from a file published on the website GitHub (https://github.com/GeorgePhillips/node-s3-url-encode/blob/master/index.js) and is licensed under the MIT License (https://github.com/GeorgePhillips/node-s3-url-encode/blob/master/LICENSE.). The original code was commited and published by George Phillips (https://github.com/GeorgePhillips) that was additionally modified by Joey Ciechanowicz.
+// node-s3-url-encode - Because s3 urls are annoying
+const encodings = {
+  "+": "%2B",
+  "!": "%21",
+  '"': "%22",
+  "#": "%23",
+  $: "%24",
+  "&": "%26",
+  "'": "%27",
+  "(": "%28",
+  ")": "%29",
+  "*": "%2A",
+  ",": "%2C",
+  ":": "%3A",
+  ";": "%3B",
+  "=": "%3D",
+  "?": "%3F",
+  "@": "%40",
+};
+
+function encodeS3URI(filename) {
+  return encodeURI(filename) // Do the standard url encoding
+    .replace(/(\+|!|"|#|\$|&|'|\(|\)|\*|\+|,|:|;|=|\?|@)/gim, function (match) {
+      return encodings[match];
+    })
+    .replace(/%20/g, "+");
+}
+
+// This function 'traverse(obj)' is modified from a snippet published on the website stackoverflow (https://stackoverflow.com/a/10460119) and is licensed as CC BY-SA 4.0 (https://creativecommons.org/licenses/by-sa/4.0/). The original stackoverflow anwser was made by El Ronnoco (https://stackoverflow.com/users/395628/el-ronnoco) that was additionally modified by user1371896 (https://stackoverflow.com/users/1371896/user1371896) and OCDev (https://stackoverflow.com/users/508558/ocdev).
 function traverse(obj) {
   var k;
   var folderlength;
@@ -31,9 +76,9 @@ function traverse(obj) {
 
     if (filetype == "file") {
       let url = urlPrefix;
-      url += path + obj;
+      url += encodeS3URI(path + obj);
 
-      body += '<a href="' + url + '">' + obj + "</a>" + "<br/>";
+      body += '<a href="' + url + '" target="_blank">' + obj + "</a>" + "<br/>";
     }
   }
 }
@@ -52,19 +97,51 @@ async function getTree() {
   document.getElementById("tree").innerHTML = body;
 }
 
-// This function 'interceptClicks()' is modified from a snippet published on the website stackoverflow (https://stackoverflow.com/a/21518470) and as such is licensed as CC BY-SA 4.0 (https://creativecommons.org/licenses/by-sa/4.0/). The original stackoverflow anwser was made by Matt Way (https://stackoverflow.com/users/277697/matt-way) that was additionally modified by user2742371.
+// This function 'interceptClicks()' is modified from a snippet published on the website stackoverflow (https://stackoverflow.com/a/21518470) and is licensed as CC BY-SA 4.0 (https://creativecommons.org/licenses/by-sa/4.0/). The original stackoverflow anwser was made by Matt Way (https://stackoverflow.com/users/277697/matt-way) that was additionally modified by user2742371.
 const interceptClicks = async () => {
   const result = await getTree();
-  if (screen.width > 992) {
-    // This part of the function is s modified from a snippet published on the website codeproject (https://www.codeproject.com/Answers/525918/Displaypluscontentplusofpluslinkplusinplusparticul#answer1). The original codeproject anwser was made by ramukhsakarp (https://www.codeproject.com/script/Membership/View.aspx?mid=7652198).
+  if (screen.width >= 992) {
+    // This part of the function is modified from a snippet published on the website codeproject (https://www.codeproject.com/Answers/525918/Displaypluscontentplusofpluslinkplusinplusparticul#answer1). The original codeproject anwser was made by ramukhsakarp (https://www.codeproject.com/script/Membership/View.aspx?mid=7652198).
     $(document).ready(function () {
       $("#tree a").click(function (e) {
         e.preventDefault();
-        $("#preview").attr("data", $(this).attr("href"));
-        $("#preview").attr(
-          "class",
-          "border border-primary rounded col-lg-7 m-1"
-        );
+
+        document
+          .getElementById("preview")
+          .setAttribute(
+            "class",
+            "invisible border border-primary rounded col-lg-7 ml-lg-1 center w-100"
+          );
+
+        // This workaround is modified from a snippet published on the website stackoverflow (https://stackoverflow.com/questions/676705/changing-data-content-on-an-object-tag-in-html) and is licenced as CC BY-SA 3.0 (https://creativecommons.org/licenses/by-sa/3.0/). The stackoverflow anwser to the question titled 'Changing data content on an Object Tag in HTML' was made by Israel Gav (https://stackoverflow.com/users/4182640/israel-gav) and was published untitled.
+        let previewObject = document.getElementById("preview");
+        previewObject.setAttribute("data", this.getAttribute("href"));
+
+        let previewClone = previewObject.cloneNode(true);
+        let previewParent = previewObject.parentNode;
+
+        previewParent.removeChild(previewObject);
+        previewParent.appendChild(previewClone);
+
+        if (
+          imageFileExtension.includes(
+            this.getAttribute("href").split(".").pop()
+          )
+        ) {
+          document
+            .getElementById("preview")
+            .setAttribute(
+              "class",
+              "border border-primary rounded col-lg-7 ml-lg-1 center w-100"
+            );
+        } else {
+          document
+            .getElementById("preview")
+            .setAttribute(
+              "class",
+              "border border-primary rounded col-lg-7 ml-lg-1 center w-100 h-100"
+            );
+        };
       });
     });
   }
